f68f5df85759b99bdd6f699c6625454526d2578d
Error <foamy125@gmail.com>
2025-11-12 17:01:14 -0600

    Add build artifacts to gitignore

--- .shadow-cljs/builds/dev-ui/release/externs.shadow.js
+++
@@ -1,6 +0,0 @@
-/** @const {ShadowJS} */ var Promise;
-/** @const {ShadowJS} */ var customElements;
-/** @const {ShadowJS} */ var fetch;
-/** @const {ShadowJS} */ var module;
-ShadowJS.prototype.reject;
-ShadowJS.prototype.stringify;
